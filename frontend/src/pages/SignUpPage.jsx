97443c2dee2169231bf94156b1475c48cf11efe3
Hamed Daly <hameddali48@gmail.com>
2025-05-07 19:10:16 +0200

    Update SignUpPage.jsx

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,7 +3,7 @@ import { useAuthStore } from "../Store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
-import AuthImagePattern from "../components/AuthImagePattern";
+import AuthImagePattern from "../components/AuthimagePattern";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
